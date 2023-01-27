1011bd821f6b87dc12c431d675be9de2599651a0
Ishant Chauhan <ishantchauhan710@gmail.com>
2023-01-27 16:59:00 +0530

    Upload photo and video modals designed

---
+++ frontend/src/components/spacefeed/spacefeed/modal/UploadVideoModal.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import {
+  Dialog,
+  Box,
+  Typography,
+  IconButton,
+  Divider,
+  TextField,
+  Avatar,
+} from "@mui/material/";
+import CloseIcon from "@mui/icons-material/Close";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+
+const UploadVideoModal = ({ open, setOpen }) => {
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <Box
+        sx={{
+          width: 500,
+        }}
+      >
+        <Box
+          paddingX={2}
+          paddingY={1}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Typography variant="h1" fontSize={20} fontWeight={600}>
+            Upload Video
+          </Typography>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        <Divider />
+        <Box padding={2}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              border: "2px dashed rgba(0,0,0,0.2)",
+              borderRadius: 2,
+              height: 300,
+              cursor: "pointer",
+            }}
+          >
+            <VideoLibraryIcon sx={{ width: 60, height: 60 }} />
+            <Typography variant="h6" fontSize={15} marginTop={1}>
+              Click here to upload a video
+            </Typography>
+          </Box>
+          <Box
+            paddingTop={2}
+            display="flex"
+            alignItems="center"
+            justifyContent="flex-end"
+          >
+            <Button
+              style={{ marginRight: "10px" }}
+              color="error"
+              variant="outlined"
+              disableElevation
+            >
+              Cancel
+            </Button>
+            <Button color="primary" variant="contained" disableElevation>
+              Upload
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Dialog>
+  );
+};
+
+export default UploadVideoModal;
--- frontend/src/pages/home/components/post/PostSectionCreatePost.jsx
+++ frontend/src/pages/home/components/post/PostSectionCreatePost.jsx
@@ -1,52 +1,16 @@
-import React from "react";
-import {
-  Box,
-  TextField,
-  Typography,
-  Avatar,
-} from "@mui/material";
+import React, { useState } from "react";
+import { Box, TextField, Typography, Avatar, Button } from "@mui/material";
 import { FcCompactCamera, FcVideoCall, FcCalendar } from "react-icons/fc";
+import UploadPhotoModal from "../../../../components/spacefeed/spacefeed/modal/UploadPhotoModal";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import UploadVideoModal from "../../../../components/spacefeed/spacefeed/modal/UploadVideoModal";
+
 const POST_TYPE_LIST_ICON_SIZE = 21;
 
-const postTypeList = [
-  {
-    label: "Photo",
-    icon: (
-      <FcCompactCamera
-        style={{
-          width: POST_TYPE_LIST_ICON_SIZE,
-          height: POST_TYPE_LIST_ICON_SIZE,
-        }}
-      />
-    ),
-  },
-  {
-    label: "Video",
-    icon: (
-      <FcVideoCall
-        style={{
-          width: POST_TYPE_LIST_ICON_SIZE,
-          height: POST_TYPE_LIST_ICON_SIZE,
-        }}
-      />
-    ),
-  },
-  {
-    label: "Event",
-    icon: (
-      <FcCalendar
-        style={{
-          width: POST_TYPE_LIST_ICON_SIZE,
-          height: POST_TYPE_LIST_ICON_SIZE,
-        }}
-      />
-    ),
-  },
-];
-
-const PostTypeChip = ({ label, icon }) => {
+const PostTypeChip = ({ postType }) => {
   return (
     <Box
+      onClick={postType.clickAction}
       sx={{
         display: "inline-flex",
         alignItems: "center",
@@ -61,20 +25,54 @@ const PostTypeChip = ({ label, icon }) => {
         },
       }}
     >
-      {icon}
+      {postType.icon}
       <Typography
         color="grey.700"
         fontSize={13}
         variant="h3"
         style={{ marginLeft: "5px" }}
       >
-        {label}
+        {postType.label}
       </Typography>
     </Box>
   );
 };
 
 const PostSectionCreatePost = () => {
+  const [showUploadPhotoModal, setShowUploadPhotoModal] = useState(false);
+  const [showUploadVideoModal, setShowUploadVideoModal] = useState(false);
+
+  const postTypeList = [
+    {
+      label: "Photo",
+      icon: (
+        <FcCompactCamera
+          style={{
+            width: POST_TYPE_LIST_ICON_SIZE,
+            height: POST_TYPE_LIST_ICON_SIZE,
+          }}
+        />
+      ),
+      clickAction: () => {
+        setShowUploadPhotoModal(true);
+      },
+    },
+    {
+      label: "Video",
+      icon: (
+        <FcVideoCall
+          style={{
+            width: POST_TYPE_LIST_ICON_SIZE,
+            height: POST_TYPE_LIST_ICON_SIZE,
+          }}
+        />
+      ),
+      clickAction: () => {
+        setShowUploadVideoModal(true);
+      },
+    },
+  ];
+
   return (
     <Box padding={1}>
       <Box display="flex" alignItems="flex-start">
@@ -99,15 +97,30 @@ const PostSectionCreatePost = () => {
         </Box>
       </Box>
       <Box
+        marginTop={2}
         display="flex"
         alignItems="center"
-        justifyContent="flex-start"
-        marginTop={2}
+        justifyContent="space-between"
       >
-        {postTypeList.map((postType,i) => (
-          <PostTypeChip key={i} label={postType.label} icon={postType.icon} />
-        ))}
+        <Box display="flex" alignItems="center" justifyContent="flex-start">
+          {postTypeList.map((postType, i) => (
+            <PostTypeChip key={i} postType={postType} />
+          ))}
+        </Box>
+        <Box>
+          <Button variant="contained" disableElevation>
+            Post
+          </Button>
+        </Box>
       </Box>
+      <UploadPhotoModal
+        open={showUploadPhotoModal}
+        setOpen={setShowUploadPhotoModal}
+      />
+      <UploadVideoModal
+        open={showUploadVideoModal}
+        setOpen={setShowUploadVideoModal}
+      />
     </Box>
   );
 };
