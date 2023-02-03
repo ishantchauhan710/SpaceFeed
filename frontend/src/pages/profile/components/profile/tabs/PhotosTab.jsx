1511841010a698c30e38fc4e2aba30cb5c9516f8
Ishant Chauhan <ishantchauhan710@gmail.com>
2023-02-03 13:55:51 +0530

    Show photos in profile page tab feature implemented

--- frontend/src/pages/profile/components/profile/tabs/PhotosTab.jsx
+++ frontend/src/pages/profile/components/profile/tabs/PhotosTab.jsx
@@ -2,80 +2,39 @@ import React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { Box } from "@mui/material";
-
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-  },
-];
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import { showImageModal } from "../../../../../states/other/imageModalSlice";
 
 const PhotosTab = () => {
+  const posts = useSelector((state) => state.profile.posts);
+  const imagePosts = posts.filter((post) => post.mediaLink != "");
+
+  useEffect(() => {
+    console.log("Posts: " + JSON.stringify(posts));
+    console.log("Image Posts: " + JSON.stringify(imagePosts));
+  }, []);
+
+  const dispatch = useDispatch();
+
   return (
     <Box paddingBottom={1}>
-      <ImageList cols={3} >
-        {itemData.map((item) => (
-          <ImageListItem key={item.img}>
-            <div className="photo-tab-image-container">
+      <ImageList cols={3}>
+        {imagePosts &&
+          imagePosts.length > 0 &&
+          imagePosts.map((item) => (
+            <ImageListItem key={item._id}>
               <img
-                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                alt={item.title}
+                src={`${item.mediaLink}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.mediaLink}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt="post item"
                 loading="lazy"
-                style={{ cursor: "pointer", width: "100%", borderRadius: 7 }}
+                style={{ height: 200, cursor: "pointer" }}
+                onClick={() => dispatch(showImageModal(item.mediaLink))}
               />
-              <div class="photo-tab-image-overlay">
-                <div class="photo-tab-image-text">
-                  39 Likes
-                </div>
-              </div>
-            </div>
-          </ImageListItem>
-        ))}
+            </ImageListItem>
+          ))}
       </ImageList>
     </Box>
   );
