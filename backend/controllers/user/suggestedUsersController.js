74760bf765aa616f0a4ae22a66ceaa7fba73f963
Ishant Chauhan <ishantchauhan710@gmail.com>
2023-02-03 17:52:56 +0530

    Suggested users controller logic changed

--- backend/controllers/user/suggestedUsersController.js
+++ backend/controllers/user/suggestedUsersController.js
@@ -3,14 +3,16 @@ const UserModel = require("../../models/userModel");
 
 const suggestedUsersController = async (req, res, next) => {
   try {
-    const suggestedUsers = await UserModel.find(
-      { _id: { $ne: req.session.userId } },
-      null,
-      {
-        limit: 10,
-      }
-    );
-    return res.status(201).json({ suggestedUsers: suggestedUsers });
+    const user = await UserModel.findById(req.session.userId);
+    const suggestedUsers = await UserModel.aggregate([
+      { $sample: { size: 11 } },
+    ]);
+
+    const filteredSuggestedUsers = suggestedUsers
+      .filter((item) => item.email !== user.email)
+      .slice(0, 10);
+
+    return res.status(200).json({ suggestedUsers: filteredSuggestedUsers });
   } catch (err) {
     next(err);
   }
